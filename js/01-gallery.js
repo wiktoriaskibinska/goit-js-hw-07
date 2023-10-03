3adfadc093b29913e0a668ea4296c8aa43f6d738
Wiktoria Skibińska <wikas2000@icloud.com>
2023-10-03 16:49:41 +0200

    added html in js fileto create simple gallery of images

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
+// creating html for each img:
+function addGalleryItems(items) {
+    return items.map(item => `<li>
+    <div class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</div>
+</li>`).join("");
+}
+gallery.innerHTML = addGalleryItems(galleryItems);
+
+// implementig of lightbox:
+
+
 
 console.log(galleryItems);
